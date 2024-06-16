a62c0723e94b8104232462e840fcbcc697e6ef0d
rodrigorobl <rodrigo.augusto.r21@gmail.com>
2024-06-16 19:43:40 -0300

    LookApp-native

---
+++ src/pages/Home/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+import styles from '../../styles/home';
+
+const Home = () => {
+  return (
+    <View
+        style={styles.block}>
+      <Text>Home</Text>
+    </View>
+  );
+};
+
+
+
+export default Home;
